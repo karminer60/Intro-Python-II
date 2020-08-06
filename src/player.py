9d46fd1d688fbb2444dc6c8793b870e19e905ed6
karina-rodriguez <karminer60@gmail.com>
2020-08-06 15:55:18 -0700

    adding inventory/item storage capabilities

--- src/player.py
+++ src/player.py
@@ -1,8 +1,12 @@
 # Write a class to hold player information, e.g. what room they are in
 # currently.
 class Player : 
-    def __init__ (self, name, room):
+    def __init__ (self, name, room, inventory = []):
         self.name = name
         self.room = room
-        
+        self.inventory = inventory 
+
+    def add_item9self, item):
+        self.inventory.append(item)
+        return "" 
 
