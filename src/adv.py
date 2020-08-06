0c04d442a0b5a5a759e9ca9a452f804155ec8923
Karina <GIT SERVICE EMAIL>
2020-08-05 19:57:02 -0700

    working on adv.py

--- src/adv.py
+++ src/adv.py
@@ -49,3 +49,9 @@ room['treasure'].s_to = room['narrow']
 # Print an error message if the movement isn't allowed.
 #
 # If the user enters "q", quit the game.
+
+for i in outside:
+    print (currentRoom)
+    print(currentDescription)
+    input()
+    
